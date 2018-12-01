a8edf4a739aa3a0c92d949571ab4ff26f61e95a7
Marcin Rybicki <m.rybicki1112@gmail.com>
2018-12-01 15:28:51 +0100

    [change] - added toString in Animal

--- src/main/java/pl/rybicki/marcin/my_git_test/Animal.java
+++ src/main/java/pl/rybicki/marcin/my_git_test/Animal.java
@@ -3,4 +3,11 @@ package pl.rybicki.marcin.my_git_test;
 public class Animal {
 
     private String name;
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
