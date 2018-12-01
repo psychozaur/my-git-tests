71a8398b2c0eaed314cad8dedf2ce22d0ec7a860
Marcin Rybicki <m.rybicki1112@gmail.com>
2018-12-01 15:11:10 +0100

    [change] - changed to imie2

--- src/main/java/pl/rybicki/marcin/my_git_test/Dog.java
+++ src/main/java/pl/rybicki/marcin/my_git_test/Dog.java
@@ -7,7 +7,7 @@ public class Dog extends Animal {
     @Override
     public String toString() {
         return "Doggo{" +
-                "imie='" + name + '\'' +
+                "imie2='" + name + '\'' +
                 '}';
     }
 }
